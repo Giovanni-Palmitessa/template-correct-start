a0b82aecb2b93fd864f6fc31325033bca68f308a
GiovanniPalmitessa <124522055+Giovanni-Palmitessa@users.noreply.github.com>
2023-07-06 15:56:21 +0200

    Initial commit

---
+++ database/migrations/2023_07_06_133841_create_portfolios_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 100);
+            $table->string('client_name', 100);
+            $table->string('url_image', 400);
+            $table->date('pickup_date');
+            $table->date('deploy_date');
+            $table->text('description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portfolios');
+    }
+};
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,38 @@
+@extends('guest.layouts.base')
+
+@section('contents')
+<form method="POST" action="{{ route('login') }}">
+    @csrf
+
+    <div class="mb-3">
+      <label for="email" class="form-label">Email address</label>
+      <input type="email" 
+      class="form-control" 
+      id="email" 
+      aria-describedby="emailHelp" 
+      name="email" 
+      value="{{ old('email') }}" 
+      required autofocus autocomplete="username">
+    </div>
+
+    <div class="mb-3">
+      <label for="password" class="form-label">Password</label>
+      <input type="password" 
+      class="form-control" 
+      id="password" 
+      name="password" 
+      required autocomplete="current-password">
+    </div>
+
+    <div class="mb-3 form-check">
+      <input type="checkbox" class="form-check-input" id="remember" name="remember">
+      <label class="form-check-label" for="remember">Remember me</label>
+    </div>
+
+    <a href="{{ route('password.request') }}">
+        'Forgot your password?'
+    </a>
+
+    <button type="submit" class="btn btn-primary">Log in</button>
+</form>
+@endsection
